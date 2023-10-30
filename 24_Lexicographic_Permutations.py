20b90539cff9e693750b4995490d232a9d33a75a
Brandon Smith <bpsmith0295@gmail.com>
2023-10-29 18:22:35 -0600

    updating 24

--- 24_Lexicographic_Permutations.py
+++ 24_Lexicographic_Permutations.py
@@ -1,29 +1,42 @@
-def increment_by_one(num_set, base):
-    def recursive_increment(num_set, index):
-        if index < 0:
-            # If we've reached the leftmost digit and still need to increment, 
-            # we need to extend the list by doubling it.
-            num_set.insert(0, 0)
-            index = 0
+class PTree:
+    root_number = None
+    TreeList = []
+    def __init__(self,root_number: int, child_numbers, len_of_child_numbers,list_so_far:list[int]) -> None:
+        self.root_number = root_number
+        while(len_of_child_numbers> 0):
+            if len_of_child_numbers ==1:
+                new_root = child_numbers[0]
+                list_so_far.append(new_root)
+                self.TreeList.append(PTree(new_root,[],0,list(list_so_far)))
+            else:
+                for i in range(0, len_of_child_numbers-1):
+                    new_root = child_numbers[i]
+                    the_rest = child_numbers[:i] + child_numbers[i + 1:]
+                    list_so_far.append(new_root)
+                    self.TreeList.append(PTree(new_root,the_rest,len(the_rest),list(list_so_far)))
+        if len_of_child_numbers ==0:
 
-        current_value = num_set[index]
-        current_value += 1
+            print(''.join(list(map(str,list_so_far))))
 
-        if current_value == base:
-            # If the current digit becomes equal to the base, set it to 0 and
-            # increment the next digit recursively.
-            num_set[index] = 0
-            recursive_increment(num_set, index - 1)
-        else:
-            # If the current digit is less than the base, we can simply update it.
-            num_set[index] = current_value
+class PermuationWorker:
+    def __init__(self, permutation_numbers) -> None:
+        tree = self.GeneratePermutationTree(permutation_numbers)
 
-    recursive_increment(num_set, len(num_set) - 1)
-    return num_set
+    def GeneratePermutationTree(self, permuation_numbers) -> PTree:
+        #for i in range(0, len(permuation_numbers)-1):
+        root = permuation_numbers[0]
+        rest = permuation_numbers[1:]
+        list_so_far = [root]
+        copy = list(list_so_far)
+        p = PTree(root,rest,len(rest),copy)
+        print()
 
-base = 10  # Change the base as needed
-n = [0]
-print(n)
-for i in range(20):
-    n = increment_by_one(n, base)
-    print(n)
+
+
+
+permutation_numbers = [1, 2, 3]
+
+p = PermuationWorker(permutation_numbers)
+print()
+
+
